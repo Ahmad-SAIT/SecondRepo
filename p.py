da613f08fcac8caab44a47d331d6bb39a2e1f9d1
Ahmad <ahmad.almasri@sait.ca>
2023-11-30 08:11:12 -0700

    done B part

--- p.py
+++ p.py
@@ -5,5 +5,65 @@
 
 #===================
 #Part for B's code:
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
+print('Okay From B')
 
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+def fun1():
+    return -1
+
+
+for i in range(100):
+    i *= 2
+    print('Value of I', i)
 #===================
